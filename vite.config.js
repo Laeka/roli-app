8b3966eb9487f22e4136a7d3fb98481d8c7c5713
Laeka <ale.tct16@gmail.com>
2025-05-16 12:39:19 +0200

    fix env vite config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import process from 'node:process';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
         configure: (proxy) => {
           // Más logging para debugging
-          console.log('Configurando proxy para /api -> http://localhost:5001');
-          
+          console.log(`Configurando proxy para /api -> ${env.VITE_API_URL}`);
           // Proxy websockets
           proxy.on('error', (err) => {
             console.error('Proxy error:', err);
@@ -44,5 +46,6 @@ export default defineConfig({
         }
       }
     }
+  }
   }
 });
